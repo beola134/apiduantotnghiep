924d1b203de12a028397221fdbed4b51ce5b1edf
beola134 <nguyentantai612004@gmail.com>
2024-10-08 20:02:46 +0700

    test

--- routes/cateRouters.js
+++ routes/cateRouters.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const cateController = require("../controllers/cateController");
 
+///tất cả các api liên quan đến danh mục
+//show danh mục theo id
+//http://localhost:5000/cate/catebyid/1
+
 //show tất cả danh mục
 //http://localhost:5000/cate/allcate
 router.get("/allcate", cateController.getAllCates);
