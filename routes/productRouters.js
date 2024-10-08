da48dca8a4ccbf20cb3ee65a53b5cba444f77319
beola134 <nguyentantai612004@gmail.com>
2024-10-08 16:44:57 +0700

    chỉnh đường dẫn trang web

--- routes/productRouters.js
+++ routes/productRouters.js
@@ -15,36 +15,36 @@ router.get("/allsp/gioitinh", productController.getCatesByGender);
 //http://localhost:5000/product/allsp/doi"
 router.get("/allsp/doi", productController.getCatesByCouple);
 
-//show sản phẩm theo danh mục
-//http://localhost:5000/product/cate/:id
-router.get("/cate/:id", productController.getProductsByCate);
-
 //show sản phẩm mới trong danh mục
 //http://localhost:5000/product/productnew/
 router.get("/productnew/", productController.getNewProductsByCategory);
 
+//show sản phẩm theo danh mục
+//http://localhost:5000/product/category/:id
+router.get("/category/:id", productController.getProductsByCate);
+
 //chi tiết sản phẩm theo id
-//http://localhost:5000/product/detailsp
-router.get("/detailsp/:id", productController.getProductById);
+//http://localhost:5000/product/chitietsp/:id
+router.get("/chitietsp/:id", productController.getProductById);
 
 //thêm sản phẩm
-//http://localhost:5000/product/addsp
-router.post("/addsp", productController.addProduct);
+//http://localhost:5000/product/themsp
+router.post("/themsp", productController.addProduct);
 
 //xóa sản phẩm
-//http://localhost:5000/product/deletesp
-router.delete("/deletesp/:id", productController.deleteProduct);
+//http://localhost:5000/product/xoasp/:id
+router.delete("/xoasp/:id", productController.deleteProduct);
 
 //sửa sản phẩm
-//http://localhost:5000/product/updatesp
-router.put("/updatesp/:id", productController.updateProduct);
+//http://localhost:5000/product/capnhatsp/:id
+router.put("/capnhatsp/:id", productController.updateProduct);
 
 //phân trang sản phẩm
-//http://localhost:5000/product/page?page=1&limit=1
-router.get("/page", productController.getProductsByPage);
+//http://localhost:5000/product/phantrang?page=1&limit=5
+router.get("/phantrang", productController.getProductsByPage);
 
 //api tìm kiếm sản phẩm bằng cách nhập tên sản phẩm và tên danh mục
-//http://localhost:5000/product/search
-router.post("/search", productController.searchProducts);
+//http://localhost:5000/product/timkiem
+router.post("/timkiem", productController.searchProducts);
 
 module.exports = router;
